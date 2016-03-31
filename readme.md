fca36ab944ed3f119ab6f5be95f9b2e5629d1328
Donald Gray <donald.gray@maclean.co.uk>
2016-03-31 09:33:17 +0100

    Added demo project and updated readme

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-A plugin for [ServiceStack](https://servicestack.net/) that allows requests to be tracked across multiple services. This is done via an http header. If no request identifier is found one is created and appended to incoming request and then added to outgoing response object. 
+A plugin for [ServiceStack](https://servicestack.net/) that allows requests to be tracked across multiple services. This is done via an http header. 
+If no request identifier is found one is created and appended to incoming request and then added to outgoing response object for every service.
 If a request id already exists then this is appended to the outgoing response object.
 
 # Quick Start
@@ -7,7 +8,7 @@ The plugin is added like any other. By default it has no external dependencies t
 ```csharp
 Plugins.Add(new RequestCorrelationFeature());
 ```
-Both the http header name (default: "x-mac-requestid") and request id generation method (default: [RustFlakes](https://github.com/peschkaj/rustflakes)) can be customised if required:
+Both the http header name (default: "x-mac-requestid") and request id generation method (default: [RustFlakes](https://github.com/peschkaj/rustflakes)) can be customised:
 ```csharp
 Plugins.Add(new RequestCorrelationFeature
 {
@@ -21,3 +22,11 @@ Where IIdentityGenerator has a single method that needs to be implemented. This 
 ```csharp
 string GenerateIdentity();
 ```
+
+# Demo
+There is a demo project "DemoService" within the solution. This is a console app that starts up a self-hosted app host on http://127.0.0.1:8090 with the request correlation plugin setup. 
+
+It has a single service with default routing. When calling http://127.0.0.1:8090/json/reply/demorequest with Postman/Fiddler etc it will return a response which contains the generated request id. It will also return the request id in the header.
+
+# Why?
+When used with alongside a logging strategy it will enable easier tracing of requests across multiple services.
---
+++ src/ServiceStack.Request.Correlation/DemoService/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace DemoService
+{
+    using System;
+    using System.Diagnostics;
+    using Funq;
+    using ServiceStack;
+    using ServiceStack.Request.Correlation;
+    using ServiceStack.Request.Correlation.Interfaces;
+    using ServiceStack.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceUrl = "http://127.0.0.1:8090/";
+            new AppHost(serviceUrl).Init().Start("http://*:8090/");
+            $"ServiceStack SelfHost listening at {serviceUrl} ".Print();
+            Process.Start(serviceUrl);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class AppHost : AppSelfHostBase
+    {
+        private readonly string serviceUrl;
+
+        public AppHost(string serviceUrl) : base("DemoService", typeof (DemoService).Assembly)
+        {
+            this.serviceUrl = serviceUrl;
+        }
+
+        public override void Configure(Container container)
+        {
+            SetConfig(new HostConfig
+            {
+                WebHostUrl = serviceUrl,
+                ApiVersion = "2.0"
+            });
+
+            // Default plugin with x-mac-requestId headername and Rustflakes generator
+            // Plugins.Add(new RequestCorrelationFeature());
+
+            // Customised plugin
+            Plugins.Add(new RequestCorrelationFeature
+            {
+                HeaderName = HeaderNames.RequestId,
+                IdentityGenerator = new DateTimeIdentityGenerator()
+            });
+        }
+    }
+
+    public class DemoService : Service
+    {
+        public object Any(DemoRequest demoRequest)
+        {
+            var header = Request.Headers[HeaderNames.RequestId];
+
+            $"Demo service received request with {header} value for {HeaderNames.RequestId} header".Print();
+
+            return new DemoResponse { Message = $"Request id = {header}" };
+        }
+    }
+
+    public class DemoRequest : IReturn<DemoResponse>
+    {
+    }
+
+    public class DemoResponse
+    {
+        public string Message { get; set; }
+    }
+
+    public static class HeaderNames
+    {
+        public const string RequestId = "x-my-requestId";
+    }
+
+    public class DateTimeIdentityGenerator : IIdentityGenerator
+    {
+        public string GenerateIdentity()
+        {
+            return DateTime.Now.ToString("O");
+        }
+    }
+}
