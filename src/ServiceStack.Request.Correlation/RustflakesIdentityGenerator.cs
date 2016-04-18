5e7247eee967e70da377c24bf84e7d06e03000e8
Donald Gray <donald.gray@maclean.co.uk>
2016-04-18 14:28:58 +0100

    Renamed private variables

--- src/ServiceStack.Request.Correlation/RustflakesIdentityGenerator.cs
+++ src/ServiceStack.Request.Correlation/RustflakesIdentityGenerator.cs
@@ -10,17 +10,17 @@ namespace ServiceStack.Request.Correlation
     public class RustflakesIdentityGenerator : IIdentityGenerator
     {
         // NOTE DecimalOxidation is not the fastest nor smallest but happy medium
-        private readonly DecimalOxidation _generator;
+        private readonly DecimalOxidation generator;
 
         public RustflakesIdentityGenerator()
         {
             var machineIdentifier = MachineIdentity.GetMachineIdentifier();
-            _generator = new DecimalOxidation(machineIdentifier);
+            generator = new DecimalOxidation(machineIdentifier);
         }
 
         public string GenerateIdentity()
         {
-            return _generator.Oxidize().ToString(CultureInfo.InvariantCulture);
+            return generator.Oxidize().ToString(CultureInfo.InvariantCulture);
         }
     }
 }
