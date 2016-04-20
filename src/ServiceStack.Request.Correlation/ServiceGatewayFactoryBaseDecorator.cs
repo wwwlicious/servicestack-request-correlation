4d35735d6c06f5a28a6e8c5a6c4559c6e86da5a8
Donald Gray <donald.gray@maclean.co.uk>
2016-04-20 11:02:16 +0100

    Added tests for gateway decorator

--- src/ServiceStack.Request.Correlation/ServiceGatewayFactoryBaseDecorator.cs
+++ src/ServiceStack.Request.Correlation/ServiceGatewayFactoryBaseDecorator.cs
@@ -1,23 +1,23 @@
 ﻿// This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this 
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 namespace ServiceStack.Request.Correlation
 {
     using System;
     using Extensions;
-    using Logging;
     using Web;
 
     public class ServiceGatewayFactoryBaseDecorator : ServiceGatewayFactoryBase
     {
         private readonly string headerName;
-        private ServiceGatewayFactoryBase gatewayFactory;
-        private readonly ILog log = LogManager.GetLogger(typeof(ServiceGatewayFactoryBaseDecorator));
+        private readonly ServiceGatewayFactoryBase gatewayFactory;
         private string correlationId;
 
         public ServiceGatewayFactoryBaseDecorator(string headerName, ServiceGatewayFactoryBase factory)
         {
+            headerName.ThrowIfNullOrEmpty(nameof(headerName));
+            factory.ThrowIfNull(nameof(factory));
+
             this.headerName = headerName;
             gatewayFactory = factory;
         }
@@ -33,22 +33,22 @@ namespace ServiceStack.Request.Correlation
 
         public override IServiceGateway GetGateway(Type requestType)
         {
-            log.Debug($"Setting {headerName} header to {correlationId} in decorator factory");
-
             var serviceGateway = gatewayFactory.GetGateway(requestType);
+
+            if (string.IsNullOrEmpty(correlationId))
+            {
+                return serviceGateway;
+            }
 
             var serviceClientBase = serviceGateway as ServiceClientBase;
             if (serviceClientBase == null)
             {
                 // Internal call, no need to do anything as using same request/response objects
-                log.Debug("ServiceClientBase null, setting header on requests");
                 return serviceGateway;
             }
 
-            // External call, add this to the headers collection
-            log.Debug("ServiceClientBase not null, setting header collection for new requests");
+            // External call, add this to the headers collection to be added to outgoing request object
             serviceClientBase.Headers.Set(headerName, correlationId);
-
             return serviceClientBase;
         }
     }
---
+++ test/ServiceStack.Request.Correlation.Tests/ServiceGatewayFactoryBaseDecoratorTests.cs
@@ -0,0 +1,106 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this 
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+namespace ServiceStack.Request.Correlation.Tests
+{
+    using System;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Testing;
+    using Xunit;
+
+    public class ServiceGatewayFactoryBaseDecoratorTests
+    {
+        private readonly ServiceGatewayFactoryBaseDecorator gateway;
+        private readonly ServiceGatewayFactoryBase decorated;
+        private const string HeaderName = "x-correlation-id";
+        private readonly string correlationId = Guid.NewGuid().ToString();
+
+        public ServiceGatewayFactoryBaseDecoratorTests()
+        {
+            decorated = A.Fake<ServiceGatewayFactoryBase>();
+            gateway = new ServiceGatewayFactoryBaseDecorator(HeaderName, decorated);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Ctor_Throws_IfHeaderNameNullOrEmpty(string header)
+        {
+            Assert.Throws<ArgumentNullException>(() => new ServiceGatewayFactoryBaseDecorator(header, decorated));
+        }
+
+        [Fact]
+        public void Ctor_Throws_IfFactoryBaseNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ServiceGatewayFactoryBaseDecorator(HeaderName, null));
+        }
+
+        [Fact]
+        public void GetServiceGateway_CallsUnderlyingGetServiceGateway()
+        {
+            var mockHttpRequest = new MockHttpRequest();
+            gateway.GetServiceGateway(mockHttpRequest);
+
+            A.CallTo(() => decorated.GetServiceGateway(mockHttpRequest)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void GetServiceGateway_ReturnsSelf()
+        {
+            var result = gateway.GetServiceGateway(new MockHttpRequest());
+            result.Should().Be(gateway);
+        }
+
+        [Fact]
+        public void GetGateway_CallsUnderlyingGetGateway()
+        {
+            var type = typeof (int);
+            gateway.GetGateway(type);
+
+            A.CallTo(() => decorated.GetGateway(type)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void GetGateway_ReturnsUnderlyingGetGateway_IfNoCorrelationId()
+        {
+            var type = typeof(int);
+            var serviceGateway = A.Fake<IServiceGateway>();
+            A.CallTo(() => decorated.GetGateway(type)).Returns(serviceGateway);
+
+            gateway.GetGateway(type).Should().Be(serviceGateway);
+        }
+
+        [Fact]
+        public void GetGateway_Returns_UnderlyingGetGateway_IfCorrelationIdAndNotServiceClientBase()
+        {
+            SetCorrelationId();
+            var type = typeof(int);
+            var serviceGateway = A.Fake<IServiceGateway>();
+            A.CallTo(() => decorated.GetGateway(type)).Returns(serviceGateway);
+
+            gateway.GetGateway(type).Should().Be(serviceGateway);
+        }
+
+        [Fact]
+        public void GetGateway_SetsUnderlyingServiceClientBaseHeaders_IfCorrelationIdAndServiceClientBase()
+        {
+            SetCorrelationId();
+            var type = typeof(int);
+
+            var jsonClient = new JsonServiceClient();
+            A.CallTo(() => decorated.GetGateway(type)).Returns(jsonClient);
+
+            gateway.GetGateway(type);
+            jsonClient.Headers[HeaderName].Should().Be(correlationId);
+        }
+
+        private void SetCorrelationId()
+        {
+            var mock = new MockHttpRequest();
+            mock.Headers[HeaderName] = correlationId;
+
+            gateway.GetServiceGateway(mock);
+        }
+    }
+}
